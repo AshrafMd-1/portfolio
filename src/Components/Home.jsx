c04b4a503eef4139e58df9923a3115c449ad7182
Ashraf Mohammed <ashrafmd0506@gmail.com>
2024-10-26 21:45:15 +0530

    add animation

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import arrowSvg from "../images/down-arrow.svg";
+import arrowSvg from "../images/home/down-arrow.svg";
 import PropTypes from "prop-types";
 import image from "../images/home/bg_home.jpg";
 
@@ -8,13 +8,21 @@ const imageAltText = "An abstract image with neon polygons and lights";
 const Home = ({ name, title }) => {
   return (
     <section id="home" className="min-height">
-      <img className="background" src={image} alt="" />
+      <img className="background" src={image} alt={imageAltText} />
       <div classList="home-text" className="home-data" style={{ color: "white" }}>
         <h1>{name}</h1>
         <h2>{title}</h2>
       </div>
       <div style={{ position: "absolute", bottom: "3rem", left: "50%" }}>
-        <img src={arrowSvg} style={{ height: "3rem", width: "3rem" }} alt={imageAltText} />
+        <img
+          src={arrowSvg}
+          style={{
+            height: "3rem",
+            width: "3rem",
+            animation: "1.5s ease-out 0s infinite slideUpDown",
+          }}
+          alt={"A down arrow"}
+        />
       </div>
     </section>
   );
