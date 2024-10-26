d6583e57569d3666151151ec3786c7b95fe9afe2
Ashraf Mohammed <ashrafmd0506@gmail.com>
2024-10-26 21:00:13 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import About from "./Components/About";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Portfolio from "./Components/Portfolio";
+
+import "./styles.css";
+
+const siteProps = {
+  name: "Ashraf Mohammed",
+  title: "Full Stack Developer & Data Scientist",
+  email: "ashrafmd0506@gmail.com",
+  gitHub: "Ashrafmd-1",
+  linkedIn: "ashraf-mohammed-75932823a",
+};
+
+const primaryColor = "#4E567E";
+const secondaryColor = "#D2F1E4";
+
+const App = () => {
+  return (
+    <div id="main">
+      <Header />
+      <Home name={siteProps.name} title={siteProps.title} />
+      <About />
+      <Portfolio />
+      <Footer {...siteProps} primaryColor={primaryColor} secondaryColor={secondaryColor} />
+    </div>
+  );
+};
+
+export default App;
